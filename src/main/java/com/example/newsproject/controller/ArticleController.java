30f8289957b2532928fdf24f57627aa5e4548e01
Komala <komalasaravanan84@gmail.com>
2023-02-03 16:37:57 +0100

    new file created

---
+++ src/main/java/com/example/newsproject/controller/ArticleController.java
@@ -0,0 +1,5 @@
+package com.example.newsproject.controller;
+
+public class ArticleController {
+
+}
---
+++ src/main/java/com/example/newsproject/repository/ArticleRepository.java
@@ -0,0 +1,5 @@
+package com.example.newsproject.repository;
+
+public interface ArticleRepository {
+
+}
